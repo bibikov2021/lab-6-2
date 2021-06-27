a487243f0942a05be97bf18ac63ed8e6ab3d3da7
bibikov2021 <86536258+bibikov2021@users.noreply.github.com>
2021-06-27 20:42:47 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 using namespace std;
 
+
+void func_a(int* l, int* r, double* n) {
+ cout << endl;
+//вывод меню
+ cout << "1.Ввод верхнего предела" << endl;
+ cout << "2.Ввод нижнего предела" << endl;
+ cout << "3. Изменить входящее число" << endl;
+ cout << "4. Выход из программы" << endl;
+ int number;
+//выбор из меню
+ cin >> number;
+ if (number == 1) {
+  cout << "Введите верхний предел заново: " << endl;
+  cin >> *r;
+
+ }
+ else if (number == 2) {
+  cout << "Введите нижний предел заново: " << endl;
+  cin >> *l;
+
+ }
+ else if (number == 3) {
+  cout << "Введите новое число: " << endl;
+  cin >> *n;
+ }
+ else exit(0); // выход из программы
+}
+
+
+
+
 int limit(int left, int right, double* n) {
 	while (*n < left || *n > right)
 	{
 		cout << "Число выходит за указанные лимиты, введите повторно: ";
-		cin >> *n;
+		func_a(&left,&right,n);
 	}
 
 	if (floor(*n) == *n) {
